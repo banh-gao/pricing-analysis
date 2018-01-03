410aa552bf19ee25ef7dc9f6b01eea4fa7fa20d3
Daniel Zozin <d.zozin@fbk.eu>
2018-01-03 14:46:22 +0100

    Generate unit price graph divided by nodes

--- plot.py
+++ plot.py
@@ -14,13 +14,10 @@ args = parser.parse_args()
 
 data = pd.DataFrame()
 
-labels = []
 df = pd.read_pickle(args.results)
 
 lines = [":","-.","--","-"]
 linecycler = cycle(lines)
-
-plot_data = pd.DataFrame()
 
 for label, grp in df.groupby('scarcity'):
     grp = grp.drop_duplicates(subset=['utilization']).sort_values(by=['utilization'])
@@ -35,14 +32,32 @@ for label, grp in df.groupby('scarcity'):
 
     plt.fill_between(xnew, interp(xnew), 0, alpha=0.2, color='grey', linewidth=0.5)
 
-    labels.append("scarcity=%s" % label)
-
 plt.legend(loc='upper left')
 
 plt.xlim(df['utilization'].min(), df['utilization'].max())
 plt.xlabel('utilization')
-
-plt.ylim(0, df['utility'].max() + 1)
+plt.ylim(0, 10)
 plt.ylabel('unit price')
 
 plt.savefig('welfare.pdf')
+
+plt.clf()
+
+for label, grp in df.groupby('scarcity'):
+    grp = grp.sort_values(by=['node','utilization'])
+
+    deployments = range(0, len(grp))
+
+    plt.step(deployments, grp['utility'],
+             linestyle=next(linecycler), linewidth=0.8, label="scarcity=%s" % label)
+
+    plt.fill_between(deployments, grp['utility'], 0, step="pre", alpha=0.2, color='grey', linewidth=0.5)
+
+plt.legend(loc='upper left')
+
+plt.xlim(0, max(df.groupby('scarcity').count()['utility'])-1)
+plt.xlabel('deployments')
+plt.ylim(0, 10)
+plt.ylabel('unit price')
+
+plt.savefig('welfare_cluster.pdf')
